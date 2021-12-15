854d7ccb4828bb0668bcb87e1372d8b42324a969
Igor Okulist <okigan@gmail.com>
2021-12-14 20:40:21 -0800

    day 13

---
+++ 2021/day13/solve1.py
@@ -0,0 +1,72 @@
+#! /usr/bin/env python
+
+print('hello')
+
+import os
+import sys
+import re
+
+from functools import * 
+from collections import *
+from itertools import * 
+
+def main():
+    dots = []
+    folds = []
+    with open('./input.txt', 'rt') as f:
+        while True:
+            line = f.readline().strip()
+            if len(line) == 0:
+                break
+            coord = re.split(',', line)
+            coord = list(map(int, coord))
+            dots += [coord]
+
+        while True:
+            line = f.readline().strip()
+            if len(line) == 0:
+                break
+            fold = re.split('=', line)
+            folds += [(fold[0], int(fold[1]))]
+
+    maxC, maxR = -float('inf'), -float('inf')
+
+    for x,y in dots:
+        maxC = max(maxC, x)
+        maxR = max(maxR, y)
+
+    m = [None] * (maxR + 1)
+    for r in range(maxR + 1):
+        m[r] = [0] * (maxC + 1)
+
+    for c,r in dots:
+        m[r][c] = 1
+
+    for fold_dir, pos in folds:
+        if fold_dir == 'fold along y':
+            for r in range(pos, len(m)):
+                offset = r - pos 
+                for c in range(0, len(m[r])):
+                    m[pos - offset][c] |= m[r][c]
+            del m[pos:]
+        x = sum(chain(*m))
+        
+        if fold_dir == 'fold along x':
+            for r, ROW in enumerate(m):
+                for c in range(pos + 1, len(ROW)):
+                    offset = c - pos
+                    m[r][pos - offset] |= m[r][c]
+                del ROW[pos:]
+        break
+
+
+    x = sum(chain(*m))
+
+
+
+    print(x)
+
+
+ 
+
+print(main())   
---
+++ 2021/day13/xx1.py
@@ -0,0 +1,55 @@
+import re
+
+with open('sample.txt', 'r') as file:
+    raw_data = file.read()
+
+
+def parse_input(raw_data):
+    points = [
+        (int(x), int(y))
+        for x, y in re.findall(r'(\d+),(\d+)', raw_data)
+    ]
+    folds = [
+        (axis, int(n))
+        for axis, n in re.findall(r'fold along ([xy])=(\d+)', raw_data)
+    ]
+    return points, folds
+
+
+POINTS, FOLDS = parse_input(raw_data)
+
+
+def make_fold(axis, line, points):
+    res = set()
+    if axis == 'x':
+        for x, y in points:
+            nx = 2 * line - x if x > line else x
+            res.add((nx, y))
+    else:
+        for x, y in points:
+            ny = 2 * line - y if y > line else y
+            res.add((x, ny))
+    return res
+
+
+def part_one():
+    return len(make_fold(*FOLDS[0], set(POINTS)))
+
+
+def part_two():
+    points = set(POINTS)
+    for fold in FOLDS:
+        points = make_fold(*fold, points)
+
+    max_x, max_y = max(x for x, _ in points), max(y for _, y in points)
+
+    grid = [[' ' for _ in range(max_x + 1)] for _ in range(max_y + 1)]
+    for j, i in points:
+        grid[i][j] = '#'
+
+    for row in grid:
+        print(''.join(row))
+
+
+print(f"Part 1: {part_one()}")  # 701
+print(f"Part 2: {part_two()}")  # FPEKBEJL
